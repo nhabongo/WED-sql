9abe8d0efd7ea6d65b8607ace5dc3285f5b5ee2c
nhabongo <nhabongo@gmail.com>
2015-10-01 17:34:50 -0400

    limitation of pl/pgsl on using NEW var to create a dynamic query

--- WED-engine/WED_tables.sql
+++ WED-engine/WED_tables.sql
@@ -1,19 +1,22 @@
 --multiples instance of a particular wedflow
 
+DROP TABLE IF EXISTS WED_attr;
+DROP TABLE IF EXISTS WED_trace;
 DROP TABLE IF EXISTS WED_flow;
+
 CREATE TABLE WED_flow (
     wid     SERIAL NOT NULL,
     PRIMARY KEY(wid)
 );
 
-DROP TABLE IF EXISTS WED_trace;
+
 CREATE TABLE WED_trace (
     wid     INTEGER NOT NULL,
     timestamp   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (wid) REFERENCES WED_flow (wid) ON DELETE RESTRICT
 );
 
-DROP TABLE IF EXISTS WED_attr;
+
 CREATE TABLE WED_attr (
     aid     SERIAL NOT NULL,
     name    TEXT,
