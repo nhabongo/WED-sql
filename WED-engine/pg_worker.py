7f778ce753a58e9f2f7b5f0c60bb4f65379ec89e
nhabongo <nhabongo@gmail.com>
2015-11-10 17:00:47 -0500

    auto include worker on postgresql.conf

--- WED-engine/pg_worker.py
+++ WED-engine/pg_worker.py
@@ -5,17 +5,24 @@ path = sys.argv[1]
 worker_name = sys.argv[2]
 with open(path,'r') as f:
     regex = re.compile(r"^#?(shared_preload_libraries)\s*=\s*'(.*)'")
+    new_file = ''
     for l in f:
-        l = l.rstrip()
         result = regex.match(l)
         if result:
             print(l.rstrip('\n'))
             workers = result.group(2).replace(' ','')
             param = result.group(1)
             if workers:
-                print(param+' = \''+workers+','+worker_name+'\'')
+                new_line = param+' = \''+workers+','+worker_name+'\'\n'
+                new_file += new_line
+                print(new_line)
             else:
-                print(param+' = \''+worker_name+'\'')
+                new_line = param+' = \''+worker_name+'\'\n'
+                new_file += new_line
+                print(new_line)
+        else:
+            new_file += l
 
-    
+with open(path,'w') as f:
+    f.write(new_file)
 
